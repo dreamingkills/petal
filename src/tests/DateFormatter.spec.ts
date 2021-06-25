7b1b41aa4b673316c67dfc73c3bbe45882d37feb
olivia-hye <realrtfl@gmail.com>
2021-06-25 03:44:11 -0400

    consistent test names

--- src/tests/DateFormatter.spec.ts
+++ src/tests/DateFormatter.spec.ts
@@ -10,7 +10,7 @@ describe(`Date Formatter`, () => {
     service = new DateFormatter();
   });
 
-  it("Should correctly format verbose dates with years and ordinal indicators", () => {
+  it("Should correctly format verbose dates with years and ordinal indicators.", () => {
     expect(
       service.formatDate(new Date(`September 20, 2003`), {
         includeYear: true,
@@ -20,7 +20,7 @@ describe(`Date Formatter`, () => {
     ).to.equal(`September 20th, 2003`);
   });
 
-  it("Should correctly format verbose dates with years and without ordinal indicators", () => {
+  it("Should correctly format verbose dates with years and without ordinal indicators.", () => {
     expect(
       service.formatDate(new Date(`November 20, 2002`), {
         includeYear: true,
@@ -29,7 +29,7 @@ describe(`Date Formatter`, () => {
     ).to.equal(`November 20, 2002`);
   });
 
-  it("Should correctly format verbose dates without years and with ordinal indicators", () => {
+  it("Should correctly format verbose dates without years and with ordinal indicators.", () => {
     expect(
       service.formatDate(new Date(`November 22, 2020`), {
         verbose: true,
@@ -38,19 +38,19 @@ describe(`Date Formatter`, () => {
     ).to.equal(`November 22nd`);
   });
 
-  it("Should correctly format verbose dates without years or ordinal indicators", () => {
+  it("Should correctly format verbose dates without years or ordinal indicators.", () => {
     expect(
       service.formatDate(new Date(`September 6, 2020`), { verbose: true })
     ).to.equal(`September 6`);
   });
 
-  it("Should correctly format non-verbose dates with years", () => {
+  it("Should correctly format non-verbose dates with years.", () => {
     expect(
       service.formatDate(new Date(`June 18, 2021`), { includeYear: true })
     ).to.equal(`2021/06/18`);
   });
 
-  it("Should correctly format non-verbose dates without years", () => {
+  it("Should correctly format non-verbose dates without years.", () => {
     expect(service.formatDate(new Date(`December 20, 2020`))).to.equal(`12/20`);
   });
 });
