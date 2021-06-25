d46eb10c5101dadd2ee07977a9b5926a0e428001
olivia-hye <realrtfl@gmail.com>
2021-06-25 03:59:17 -0400

    fix punctuation in l10n tests

--- src/tests/Locales.spec.ts
+++ src/tests/Locales.spec.ts
@@ -10,17 +10,17 @@ describe(`Locales`, () => {
     service = new LocaleHandler();
   });
 
-  it(`Should correctly localize with variables`, () => {
+  it(`Should correctly localize with variables.`, () => {
     expect(service.getLocale(`en`, `general.PING`, { name: `Test` })).to.equal(
       `Hello, Test!`
     );
   });
 
-  it(`Should correctly localize English`, () => {
+  it(`Should correctly localize English.`, () => {
     expect(service.getLocale(`en`, `general.PING`)).to.equal(`Hello, $name!`);
   });
 
-  it(`Should correctly localize Korean`, () => {
+  it(`Should correctly localize Korean.`, () => {
     expect(service.getLocale(`ko`, `general.PING`)).to.equal(
       `안녕하세요, $name!`
     );
@@ -32,7 +32,7 @@ describe(`Locales`, () => {
     );
   });
 
-  it(`Should correctly fallback to error phrase if the fallback fails .`, () => {
+  it(`Should correctly fallback to error phrase if the fallback fails.`, () => {
     // @ts-ignore - have to ignore this so that it intentionally fails work
     expect(service.getLocale(`ko`, `general.NONEXISTENT_TRANSLATION`)).to.equal(
       `This text is untranslated, and a fallback could not be achieved.`
