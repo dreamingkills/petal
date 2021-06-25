2e9aba876d78f835051929cac957cd08ae2baf2a
olivia-hye <realrtfl@gmail.com>
2021-06-25 05:01:48 -0400

    set Command properties to readonly

--- src/structures/command/Command.ts
+++ src/structures/command/Command.ts
@@ -5,14 +5,14 @@ import { LocaleHandler } from "../../services/Locales";
 import { Bot } from "../Bot";
 
 export abstract class Command {
-  name: string = this.constructor.name
+  readonly name: string = this.constructor.name
     .toLowerCase()
     .slice(0, -"Command".length);
-  aliases: string[] = [];
+  readonly aliases: string[] = [];
 
-  client = container.get<Client>(TYPES.Client);
-  bot = container.get<Bot>(TYPES.Bot);
-  locale = container.get<LocaleHandler>(TYPES.LocaleHandler);
+  readonly client = container.get<Client>(TYPES.Client);
+  readonly bot = container.get<Bot>(TYPES.Bot);
+  readonly locale = container.get<LocaleHandler>(TYPES.LocaleHandler);
 
   abstract run(message: Message): Promise<unknown>;
 }
